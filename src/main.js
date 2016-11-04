60be33c1f1e4431d2cf519417c8bc7afb917961b
Mauricio Andreazza <mauricio.webdev@gmail.com>
2016-11-04 15:07:14 -0200

    created todo-form component

--- src/main.js
+++ src/main.js
@@ -5,12 +5,34 @@ Vue.component('todo-item', {
   props: ['todo'],
   template: '<li>{{ todo.text }}</li>'
 })
+
+Vue.component('todo-form', {
+  props: ['todo'],
+  template: '<div><input type="text" @keyup.enter="newTodo" v-model="taskName" placeholder="Task" />\n' +
+  '<button @click="save" type="button">New Todo</button></div>',
+  data: function () {
+    return {
+      taskName: ''
+    }
+  },
+  watch: {
+    taskName: function () {
+      this.$emit('typing')
+    }
+  },
+  methods: {
+    save: function () {
+      this.$emit('save', this.taskName)
+      this.taskName = ''
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   data: {
-    status: 'Please, enter a new task',
+    status: 'Please, enter a new task now',
     message: 'ola',
-    taskName: '',
     id: 'bindou',
     seen: true,
     url: 'http://www.terra.com.br',
@@ -21,15 +43,13 @@ new Vue({
       { text: 'Task4' }
     ]
   },
-  watch: {
-    taskName: function () {
-      this.status = 'Typing...'
-    }
-  },
   methods: {
-    newTodo: function () {
-      this.todos.push({text: this.taskName})
+    newTodo: function (name) {
+      this.todos.push({text: name})
       this.taskName = ''
+    },
+    typing: function () {
+      this.status = 'Typing...'
     }
   }
 })
